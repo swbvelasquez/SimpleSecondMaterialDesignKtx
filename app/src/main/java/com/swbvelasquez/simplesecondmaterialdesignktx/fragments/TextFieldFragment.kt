e273b1cfcc52ce5f2da1aac009221566132cbe41
swbvelasquez <swbvelasquez@gmail.com>
2023-05-03 13:55:24 -0500

    Text field parte 3

--- app/src/main/java/com/swbvelasquez/simplesecondmaterialdesignktx/fragments/TextFieldFragment.kt
+++ app/src/main/java/com/swbvelasquez/simplesecondmaterialdesignktx/fragments/TextFieldFragment.kt
@@ -1,12 +1,14 @@
 package com.swbvelasquez.simplesecondmaterialdesignktx.fragments
 
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.widget.addTextChangedListener
 import com.swbvelasquez.simplesecondmaterialdesignktx.R
-import com.swbvelasquez.simplesecondmaterialdesignktx.databinding.FragmentButtonBinding
 import com.swbvelasquez.simplesecondmaterialdesignktx.databinding.FragmentTextFieldBinding
 import com.swbvelasquez.simplesecondmaterialdesignktx.utils.Constants
 import com.swbvelasquez.simplesecondmaterialdesignktx.utils.Data
@@ -25,6 +27,25 @@ class TextFieldFragment : Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         binding = FragmentTextFieldBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        with(binding) {
+            tilCapitalLetter.setEndIconOnClickListener {
+                if (etCapitalLetter.text != null) {
+                    val contentStr = etCapitalLetter.text.toString()
+                    etCapitalLetter.setText(contentStr.uppercase())
+                }
+            }
+
+            etPrice.addTextChangedListener { text ->
+                if(text.isNullOrEmpty() || text.toString().toInt() < 5){
+                    etPrice.error = getString(R.string.error_price_min)
+                }
+            }
+        }
     }
 
     companion object {
